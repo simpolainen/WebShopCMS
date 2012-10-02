5557f2d7ff736f27b7f7cd04644a99b83d626592
a09sebes <sebastianespling@gmail.com>
2012-10-02 22:57:58 +0200

    fix
    xcv

--- WebShopCMS/Scripts/menuToggle.js
+++ WebShopCMS/Scripts/menuToggle.js
@@ -3,7 +3,6 @@ $(function() {
     $("li.content").hide();
     $("ul.nav").delegate("li.toggle", "click", function() { 
         $(this).next().toggle("fast").siblings(".content").hide("fast");
-        alert();
     });
 });
 });
