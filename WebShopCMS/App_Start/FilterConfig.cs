311fd20950876dc827aa56a7aef951be74b6bc0e
Simon Gullberg <simongullbergs@gmail.com>
2012-10-14 13:35:30 +0200

    data Signed-off-by: Simon Gullberg <simongullbergs@gmail.com>

--- WebShopCMS/App_Start/FilterConfig.cs
+++ WebShopCMS/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace WebShopCMS
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute());dasd
         }
     }
 }
