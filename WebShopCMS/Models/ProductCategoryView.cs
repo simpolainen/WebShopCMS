5de6b512e331732c1e9c7fc3116cc162e8f415a6
Simon Gullberg <simongullbergs@gmail.com>
2012-10-14 15:45:56 +0200

    dsd
    Signed-off-by: Simon Gullberg <simongullbergs@gmail.com>

---
+++ WebShopCMS/Models/ProductCategoryView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShopCMS.Models
+{
+    public class ProductCategoryView
+    {
+        public Product Product { get; set; }
+        public Categories Category { get; set; }
+    }
+}
